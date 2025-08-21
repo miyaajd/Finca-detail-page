11c4b96e833b2e5f4e95f24e954526aef4209419
miyaajd <ehdal1032@gmail.com>
2025-08-21 21:10:27 +0900

    more-build

--- js/main.js
+++ js/main.js
@@ -11,14 +11,26 @@ window.addEventListener("load", function () {
   const q = 154000;
   //
   won.innerText = `0 원`;
-  // 
+  //
   sizes.addEventListener("change", () => {
     if (sizes.value === "ss") {
-      won.innerText =`${s.toLocaleString()} 원`;
+      won.innerText = `${s.toLocaleString()} 원`;
     } else if (sizes.value === "qk") {
       won.innerText = `${q.toLocaleString()} 원`;
     } else {
       won.innerText = `0 원`;
     }
+  });
+  //
+  //
+  const moreBtn = this.document.querySelectorAll(".more li");
+  const moreDscr = this.document.querySelectorAll(".more-dscr li");
+  moreBtn.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      moreDscr.forEach((dscr, index) => {
+        dscr.classList.remove("active");
+      });
+      moreDscr[index].classList.toggle("active");
+    });
   });
 });
