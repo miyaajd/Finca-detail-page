f508db65817dd1131842956a379f6003ea360a36
miyaajd <ehdal1032@gmail.com>
2025-08-21 18:30:00 +0900

    account js done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 window.addEventListener("load", function () {
-    const swiper = new Swiper(".mainLeft", {
-      scrollbar: {
-        el: ".swiper-scrollbar",
-      },
-    });
+  const swiper = new Swiper(".mainLeft", {
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+  // account
+  const sizes = this.document.querySelector("#sizes");
+  const won = this.document.querySelector("#won");
+  const s = 104000;
+  const q = 154000;
+  //
+  sizes.addEventListener("change", () => {
+    if (sizes.value === "ss") {
+      won.innerText =`${s.toLocaleString()} 원`;
+    } else if (sizes.value === "qk") {
+      won.innerText = `${q.toLocaleString()} 원`;
+    } else {
+      won.innerText = `0 원`;
+    }
+  });
 });
